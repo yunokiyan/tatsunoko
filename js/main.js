a136d811d3d678f5e664f2b25df92fb4459f92c1
yunokiya <yu.nokiyan@gmail.com>
2021-06-08 14:09:53 +0900

    プロト作成

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var rellax = new Rellax('.js-rellax');
+
+$(function() {
+
+  var timeout = 1000;
+
+  setTimeout(function(){
+    $(".loader").hide();
+    var i = 0;
+    var canvas = setInterval(function(){
+      $("#projector").css("opacity",i/100);
+      i++
+      if(i==100){
+        clearInterval(canvas);
+        setTimeout(function(){
+          $("#wrapper").addClass("fade_on");
+        },500)
+      }
+    },5);
+  },timeout);
+
+  fade_on();
+
+});
+
+
+function fade_on(){
+  $(window).scroll(function (){
+    $(".fade").each(function(i){
+      var imgPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/5){
+        $(this).addClass("fade_on");
+      } else {
+        //$(this).removeClass("fade_on");
+      }
+    });
+  });
+}
